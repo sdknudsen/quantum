f45a24df2228ce0c6dc8f6c3f66ba51ad25e8205
Stefan Knudsen <sdknudse@ucalgary.ca>
2015-02-15 04:07:10 -0700

    Added arbitrary size control gate function

--- quantum.hs
+++ quantum.hs
@@ -1,3 +1,6 @@
+-- Author: Stefan Knudsen
+-- Description: Matrix multiplication with added features for quantum gates, targeted for 2 qubits
+
 -- https://hackage.haskell.org/package/matrix-0.2.1/docs/Data-Matrix.html
 -- x' is a matrix, x is a function that applies x'
 
@@ -7,6 +10,7 @@
 -- factor out constants
 
 import Data.Matrix
+import Data.Bits
 import Data.Char
 import Numeric
 
@@ -14,8 +18,9 @@ import Numeric
 -- mult xs = foldr1 multStd xs
 
 -- normFactor = (/sqrt 2)
--- i,x,z,h,o,l,p,m,oo,ol,lo,ll,phip,psip,psim :: Matrix Double
--- cnot,cz :: Matrix Double -> Matrix Double
+-- o,l,p,m,oo,ol,lo,ll,phip,psip,psim :: Matrix Double
+-- i,x,z,h,cnot,cz :: Matrix Double -> Matrix Double
+-- c :: Matrix Double -> Int -> Int -> Matrix Double
 -- Replace with the lines above to normalize 2 qubit matrices and vectors
 normFactor = (*1)
 o,l,p,m,oo,ol,lo,ll,phip,psip,psim :: Matrix Int
@@ -26,8 +31,8 @@ toBra x xs = fromList 1 x xs
 t = transpose
 
 -- Vectors
-o = toKet 2 [1,0]				-- | 0 >
-l = toKet 2 [0,1]				-- | 1 >
+o = toKet 2 [1,0]			-- | 0 >
+l = toKet 2 [0,1]			-- | 1 >
 p = toKet 2 $ map normFactor [1,1]	-- | + >
 m = toKet 2 $ map normFactor [1,-1]	-- | - >
 
@@ -65,12 +70,22 @@ cnot' = fromList 4 4 [1,0,0,0,
                       0,1,0,0,
                       0,0,0,1,
                       0,0,1,0]
+
+-- cnot' = fromList 4 4 [1,0,0,0,
+--                       0,1,0,0,
+--                       0,0,3,4,
+--                       0,0,4,3]
+
 cnot21 = cnot2
 cnot2 = multStd cnot2'
 cnot2' = fromList 4 4 [1,0,0,0,
                        0,0,0,1,
                        0,0,1,0,
                        0,1,0,0]
+-- cnot2' = fromList 4 4 [1,0,0,0,
+--                        0,0,0,4,
+--                        0,0,1,0,
+--                        0,4,0,0]
 
 swap = cnot12.cnot21.cnot12
 
@@ -91,19 +106,43 @@ ch' = fromList 4 4 [1,0,0,0,
 --ch2 = swap.ch.swap
 ch21 = ch2
 ch2 = ch2'
-ch2' = fromList 4 4 [1,0,0,0,
-                     0,normFactor 1,0,normFactor 1,
-                     0,0,1,0,
-                     0,normFactor 1,0,normFactor (-1)]
+ch2' = fromList 4 4 [1,	0,		0,	0,
+                     0,	normFactor 1,	0,	normFactor 1,
+                     0,	0,		1,	0,
+                     0,	normFactor 1,	0,	normFactor (-1)]
 
 
--- Kronecker products are considered functions here, so they are 'hungry'
+-- Kronecker products are considered functions here, so they're 'hungry'
 k a b = multStd $ k' a b
 
 -- The matrix or vector equivalent is k'
-k' a b = matrix ((nrows a)*(nrows b)) ((ncols a)*(ncols b)) tensorFunc
-  where tensorFunc (i,j) = a!(((i-1) `quot` (nrows b)) + 1, ((j-1) `quot` (ncols b)) + 1) *
-                           b!((i-1) `mod` (nrows b) + 1, (j-1) `mod` (ncols b) + 1)
+k' a b =
+  let tensorFunc (i,j) = a!(((i-1) `quot` (nrows b)) + 1, ((j-1) `quot` (ncols b)) + 1) *
+                     b!((i-1) `mod` (nrows b) + 1, (j-1) `mod` (ncols b) + 1)
+  in
+   matrix ((nrows a)*(nrows b)) ((ncols a)*(ncols b)) tensorFunc
+
+
+-- Control gate for more than 2 qubits
+-- Takes matrix to apply, control bit, and output bit
+-- TODO: allow for more than one control and one controlled bit
+c :: Matrix Int -> Int -> Int -> Int -> Matrix Int
+c g n x y = matrix (2^n) (2^n) (control g n (n - x) y) -- x\ ((max x y) - x + 1)
+  where
+    control :: Matrix Int -> Int -> Int -> Int -> (Int,Int) -> Int
+    control g n x y (i,j)
+      | not (bitIsOne x i j) && i == j = 1
+      | not (bitIsOne x i j) = 0
+      | otherwise = kList g n y!(i,j)
+      where
+        bitIsOne :: Int -> Int -> Int -> Bool
+        bitIsOne c i j = ((i-1) .&. (j-1) .&. 2^c) /= 0
+        kList g' n v
+          | n < 1 = error "Second argument must be at least 1"
+          | v == 1 && n == 1 = g'
+          | n == 1 && n /= v = i'
+          | n /= v = k' (kList g' (n-1) v) i'
+          | otherwise = k' (kList g' (n-1) v) g'
 
 -- Common 2 qubit vectors
 oo = k' o o
